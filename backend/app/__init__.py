ed95666d584958a9953a6575b5a27044f56c2be2
guchiyam <yamatoe1227@gmail.com>
2024-02-25 15:33:42 -0800

    final commit

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,12 +1,9 @@
 from flask import Flask
 from flask_cors import CORS
 
-from config import Config
-
-def create_app(config_class=Config):
+def create_app():
     app = Flask(__name__)
     CORS(app)
-    app.config.from_object(config_class)
 
     # register blueprints
     from app.main import bp as main_bp
