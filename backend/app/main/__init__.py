e483d950e9b96c8c7c253fbed50f4b7a71a8a25b
guchiyam <yamatoe1227@gmail.com>
2024-02-25 14:22:42 -0800

    finish project

--- backend/app/main/__init__.py
+++ backend/app/main/__init__.py
@@ -3,4 +3,3 @@ from flask import Blueprint
 bp = Blueprint('api', __name__)
 
 from app.main import routes
-from app.main import test_routes
--- backend/app/utils/query.py
+++ backend/app/utils/query.py
@@ -22,15 +22,15 @@ def query(collection, prompt):
     query_engine = index.as_query_engine()
     return query_engine.query(prompt).response
 
-def chat(collection, prompt):
-    print(collection)
-    index = RetrieveFromStorageContext(collection=collection)
-    # chat_engine = index.as_chat_engine()
-    chat_engine = index.as_chat_engine(
-        chat_mode="context",
-        # memory=memory,
-        system_prompt=(
-            "You are an education chatbot. You are given a textbook as a context. You can only answer questions that is within your context information. If you cannot answer, say that the question is out of context"
-        ),
-    )
-    return chat_engine.chat(prompt).response
+# def chat(collection, prompt):
+#     print(collection)
+#     index = RetrieveFromStorageContext(collection=collection)
+#     # chat_engine = index.as_chat_engine()
+#     chat_engine = index.as_chat_engine(
+#         chat_mode="context",
+#         # memory=memory,
+#         system_prompt=(
+#             "You are an education chatbot. You are given a textbook as a context. You can only answer questions that is within your context information. If you cannot answer, say that the question is out of context"
+#         ),
+#     )
+#     return chat_engine.chat(prompt).response
