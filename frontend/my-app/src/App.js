f5010980bec35b2fbac7825f0f47648d30bc63e7
ArenAshikian <Ashikianaren@gmail.com>
2024-02-25 06:11:39 -0800

    Added LED by default

--- frontend/my-app/src/App.js
+++ frontend/my-app/src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import Chat from './components/Chat/Chat';
 import Header from './components/Header/Header';
 import Container from './components/Container/Container';
+import MenuButton from './components/Header/MenuButton';
 
 const App = () => {
   return (
     <div className="app">
       <Header />
       <Container />
+      
     </div>
   );
 };
